37eb36480c881f5a9374e7a928204a29c9be3e76
Abhinav1213 <113532240+Abhinav1213@users.noreply.github.com>
2024-05-24 04:49:54 +0530

    Database connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("DB Connected");
+}).catch((err) => { 
+    console.log(err);
+});
 
 app.get("/", (req, res) => { 
     res.send("Wlcome Users...");

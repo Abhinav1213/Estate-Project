58fe3545dac964c0575bceb51528bca7cc28d5e7
Abhinav1213 <113532240+Abhinav1213@users.noreply.github.com>
2024-05-24 17:21:16 +0530

    created errorHandeler middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import { User } from '../models/user.model.js';
 import bcrypt from 'bcrypt';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async(req, res) => { 
+export const signup = async(req, res,next) => { 
     const { username, email, password } = req.body;
     const hashPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ username, email, password: hashPassword });
@@ -9,7 +10,6 @@ export const signup = async(req, res) => {
         await newUser.save();
         res.status(201).send("user created successfully"); 
     } catch (err) {
-        res.status(409).send(err.message);
+        next(errorHandler(err.statusCode,err.message ));
     }
-
 }

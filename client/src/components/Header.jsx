42196a2e199ae48bb9555dad36d46099158c94f8
Abhinav1213 <113532240+Abhinav1213@users.noreply.github.com>
2024-06-06 21:57:37 +0530

    Private Route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+    const { currentUser } = useSelector((state) => state.user)
+    console.log(currentUser);
     return (
         <header className="bg-gray-800 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <Link to="/" className="text-white text-lg font-bold">
                     <div className="text-white text-lg font-bold">
-                        CHIND-Estate
+                        MERN-Estate
                     </div>
                 </Link>
                 <div className="flex w-1/2 md:w-1/3 relative">
@@ -21,20 +24,33 @@ const Header = () => {
                         <FaSearch />
                     </button>
                 </div>
-                <ul className="flex text-white">
-                    <Link to="/" className="text-white text-lg font-bold">
-                        <li className="px-2 hidden sm:inline">Home</li>
-                    </Link>
-                    <Link to="/about" className="text-white text-lg font-bold">
-                        <li className="px-2 hidden sm:inline">About</li>
-                    </Link>
-                    <Link to="/signin" className="text-white text-lg font-bold">
-                        <li className="px-2 hidden sm:inline">Sign In</li>
-                    </Link>
-                    <Link to="/signup" className="text-white text-lg font-bold">
-                        <li className="px-2">Sign Up</li>
-                    </Link>
+                <ul className="flex items-center text-white space-x-4">
+                    <li>
+                        <Link to="/" className="text-white text-lg font-bold px-2 hidden sm:inline">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about" className="text-white text-lg font-bold px-2 hidden sm:inline">About</Link>
+                    </li>
+                    {currentUser === null ? (<li>
+                            <Link to="/signin" className="text-white text-lg font-bold px-2 hidden sm:inline">Sign In</Link>
+                        </li>)
+                        : (<li> 
+                                <div className="flex items-center">
+                                    <Link to="/profile">
+                                        <img src={currentUser.avatar} alt="User Avatar" className="w-8 h-8 rounded-full" />   
+                                    </Link>
+                            </div>
+                        </li>)}
+                    <li>
+                        {
+                            currentUser === null ? (<Link to="/signup" className="text-white text-lg font-bold px-2 hidden sm:inline">Sign Up</Link>)
+                            : null
+                        
+                        }
+
+                    </li>
                 </ul>
+
             </div>
         </header>
     )
